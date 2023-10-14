43d1ae6de58a7ebec79195739d90ddbafd60456f
MagdalenaWiecek <magdalena.wiecek@gmail.com>
2023-10-14 22:13:30 +0200

    moj pierwszy commit

---
+++ src/addTwoNumbers.js
@@ -0,0 +1,15 @@
+// const name = "Magda";
+// console.log(name);
+const addTwoNumbers = (num1, num2) => {
+  return num1 + num2;
+};
+
+const [liczbaPrzekazanaWTerminalu1, liczbaPrzekazanaWTerminalu2] =
+  process.argv.slice(2);
+
+const temp = addTwoNumbers(
+  liczbaPrzekazanaWTerminalu1 * 1,
+  liczbaPrzekazanaWTerminalu2 * 1
+);
+
+console.log(`Wynik dodawnia to: ${temp}`);
